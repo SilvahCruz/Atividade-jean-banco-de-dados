56b85b8f446601fa1fa6a192bdeb400cee0a7a44
SilvahCruz <167219966+SilvahCruz@users.noreply.github.com>
2025-04-28 10:24:32 -0300

    qaadsadas

--- model/AuthModel.php
+++ model/AuthModel.php
@@ -1,51 +1,24 @@
 <?php
 
-function get( $CPF, $senha){
-    $conn = new usePDO; //obrigatorio
-    $instance = $conn->getInstance(); //obrigatorio
- 
+function get($CPF, $senha) {
+    $conn = new usePDO; 
+    $instance = $conn->getInstance(); 
 
-    //criptografar a senha
-    $hashed_password = password_hash($senha, PASSWORD_DEFAULT);
- 
-    //cadastro usuário
-    $sql = "SELECT usuario (senha) VALUES (?)";
+    $sql = "SELECT u.senha FROM usuarios u
+            INNER JOIN pessoas p ON p.id = u.id_pessoa
+            WHERE p.CPF = ?";
     $stmt = $instance->prepare($sql);
-    $stmt->execute([$hashed_password]);
-   $idUsuario = $instance->lastInsertID();
+    $stmt->execute([$CPF]);
 
-
-  //cadastro pessoa
-    $sql = "SELECT pessoa (CPF) VALUES (?)";
-    $stmt = $instance->prepare($sql);
-    $stmt->execute([$CPF, $idUsuario]);
-
-
-    return $CPF;
-}
-
-// Conectar ao banco de dados
-$pdo = new PDO('mysql:host=localhost;dbname=atividade_jean', 'CPF', 'senha');
-
-// Pegar os dados de entrada
-$CPF = $_POST['CPF'];
-$senha = $_POST['senha'];
-
-// Buscar a pessoa no banco de dados
-$stmt = $pdo->prepare("SELECT * FROM pessoa WHERE CPF = ?");
-$stmt->execute([$CPF]);
-$CPF = $stmt->fetch(PDO::FETCH_ASSOC);
-
-// Verificar se o usuário existe e se a senha está correta
-if ($user && password_verify($CPF, $senha['senha'])) {
-    // A senha está correta
-    echo "Login bem-sucedido!";
-    // Aqui eu posso iniciar uma sessão ou redirecionar um usuário
-    session_start();
-    $_SESSION['user_id'] = $idUsuario['id_usuario'];
-    $_SESSION['username'] = $username['username'];
-    header('Location: ../tela-incial.php'); // Redireciona para uma página de usuário logado
-} else {
-    // Usuário ou senha inválidos
-    echo "Usuário ou senha incorretos.";
+    $user = $stmt->fetch(PDO::FETCH_ASSOC);
+    
+    if ($user) {
+        if (password_verify($senha, $user['senha'])) {
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+        return false;
+    }
 }
